e9b4cc16a5073822c29b8e85ec6fe7398dbbdc5a
Jorge Sandoval <sandovl06roque@gmail.com>
2024-04-01 16:30:43 -0700

    Agregando la pantalla de ajustes

---
+++ src/screens/Settings.js
@@ -0,0 +1,9 @@
+import MainLayout from "../components/ui/layouts/MainLayout"
+
+export default function SettingsScreen () {
+    return (
+        <MainLayout back={true}>
+
+        </MainLayout>
+    )
+}
