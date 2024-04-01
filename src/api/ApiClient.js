fe278e827af6e2ccfa8fc554efbddb8484ded35b
Jorge Sandoval <sandovl06roque@gmail.com>
2024-03-31 20:53:11 -0700

    Agregando el ENDPOINT de agregar receta favorita

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -3,7 +3,8 @@ const API_BASE_URL = "https://recipes-api-dev.koyeb.app/" //Agregar despues app/
 const ENDPOINTS = {
     AuthUser: 'user/auth/v1/login',
     PostNewUser: 'user/auth/v2/register',
-    GetUserData: (id) => `user/get-data/${id}`
+    GetUserData: (id) => `user/get-data/${id}`,
+    AddFavorite: (userId, recipeId) => `user/api/favorite/add/${userId}/recipe/${recipeId}`
 }
 
 export {
